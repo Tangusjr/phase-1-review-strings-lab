ff85cefc06110f644999d042cbf77b54a7d174f6
tangusjr <kipngenogilbert96@gmail.com>
2022-06-08 17:48:10 +0300

    commit tree d6abe059070ed8e3d989ce294e92b625fe7f9ee5

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Brevik Cheruiyot";
+const welcomeMessage = "Welcome to Flatbook, Brevik Cheruiyot!";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, BREVIK CHERUIYOT!";
+const shortGreeting = "Welcome, B!";
